d28fe410217e5d087982fbdb1804f4f08448e9a7
Martin <martin.e.mongelos@gmail.com>
2017-05-06 12:04:31 +1000

    minior changes

--- js/module/m-contactList.js
+++ js/module/m-contactList.js
@@ -12,22 +12,21 @@ $(document).ready(function() {
         $('#contactList__searchBox, #search__input').removeClass('is-hover');
     });
     
-    //get JSON
-   var box_contact;
+    var box_contact;
     $.get('contact.html', function (data_c) {
               
             box_contact = data_c;
             $.getJSON('contact-list-users.json', function(data){
         
-                $.each(data, function(i, usuario){
-                    //console.log(usuario);
+                $.each(data, function(i, user){
+                    //console.log(user);
                     $.when( $("#contactList__collapsible-list").append(box_contact)).done(function(){
                         
-                        var status_profileBox = (usuario.status == 'Offline') ? 'offBox' : 'onBox';
+                        var status_profileBox = (user.status == 'Offline') ? 'offBox' : 'onBox';
                         var obj = $("#contactList__collapsible-list .contactList__profileBox:last").addClass(status_profileBox);
-                        obj.find('.profile__user').html(usuario.username);
-                        obj.find('.profile__img').attr('src' , usuario.profileimage);
-                        var status_class = (usuario.status == 'Offline') ? 'offline' : 'online';
+                        obj.find('.profile__user').html(user.username);
+                        obj.find('.profile__img').attr('src' , user.profileimage);
+                        var status_class = (user.status == 'Offline') ? 'offline' : 'online';
                         obj.find('.profile__state .profile__state-circle').addClass(status_class);                
                         
                     });
@@ -37,4 +36,6 @@ $(document).ready(function() {
             });               
     });
            
+
+    
 });
